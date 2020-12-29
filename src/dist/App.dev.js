78f32271986413d6e71ffc94af23d8bec474f126
Wenxuan Huang <w69huang@uwaterloo.ca>
2020-12-29 12:08:20 -0500

    rearranged directory

---
+++ src/dist/App.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _reactRouterDom = require("react-router-dom");
+
+function App() {
+  return;
+}
+
+var _default = App;
+exports["default"] = _default;
